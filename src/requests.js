8a273e26ae2de43e00f44343d800444f1b0970c1
pranav2701 <pranav.vasist020701@gmail.com>
2022-05-16 22:34:32 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+const API_KEY = "cfaf6995fda2f63d20c549d2b8b074fc";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+
+export default requests;
